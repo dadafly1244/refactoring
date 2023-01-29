8460e753325139d805c054ad74570310c63de46b
양다영 <dayoung.yang@ahha.ai>
2023-01-29 12:08:58 +0900

    refactor: 주석제거

--- ch6/6-3-1.js
+++ ch6/6-3-1.js
@@ -1,5 +1,4 @@
 export function price(order) {
-  // 가격(price) = 기본가격 - 수량할인 + 배송비
   const basePrice = order.quantity * order.itemPrice
   const discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05
   const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100)
