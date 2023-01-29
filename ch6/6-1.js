5ed52479985aee3c64ee0b75203fcb6de7b4ee3b
양다영 <dayoung.yang@ahha.ai>
2023-01-29 11:22:57 +0900

    comment: 리팩토링을 위한 주석 추가

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,15 +1,18 @@
 export function printOwing(invoice) {
-  let outstanding = 0;
-
+  
+  // 배너 출력
   console.log('***********************');
   console.log('**** Customer Owes ****');
   console.log('***********************');
 
+  //총 가격 계산
   // calculate outstanding
+  let outstanding = 0;
   for (const o of invoice.orders) {
     outstanding += o.amount;
   }
 
+  //지급 날짜 계산
   // record due date
   const today = new Date();
   invoice.dueDate = new Date(
@@ -18,6 +21,7 @@ export function printOwing(invoice) {
     today.getDate() + 30
   );
 
+  //세부사항 출력
   //print details
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
