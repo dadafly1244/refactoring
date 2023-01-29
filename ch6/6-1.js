f9e95a5e58a58ac865ff6ce3be540e3ad5f6dbf4
양다영 <dayoung.yang@ahha.ai>
2023-01-29 11:37:30 +0900

    refactor: calculateOutstanding 함수 절차지향 -> reduce사용으로 변경

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,24 +1,8 @@
 export function printOwing(invoice) {
-  
   printBanner();
-
   let outstanding = calculateOutstanding(invoice);
-
-
-  //지급 날짜 계산
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
-  );
-
-  //세부사항 출력
-  //print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+  recordDueDate(invoice);
+  printDetails(invoice, outstanding)
 }
 function printBanner(){
   console.log('***********************');
@@ -26,12 +10,23 @@ function printBanner(){
   console.log('***********************');
 }
 function calculateOutstanding(invoice){
-  let outstanding = 0;
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
-  return outstanding
+  return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
 }
+
+function recordDueDate(invoice) {
+  const today = new Date();
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+}
+function printDetails(invoice, outstanding){
+  console.log(`name: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
 const invoice = {
   orders: [{ amount: 2 }, { amount: 5 }],
   customer: '엘리',
